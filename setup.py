1191eef120c5737547ea8aca454707f36e6ad923
gregor <gregor.olenik@web.de>
2014-12-16 11:37:10 +0100

    implemented py.test testing on python setup test

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
+import sys
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 package_name = 'PKP'
 # Get the version from FoamAna/version.py without importing the package
 exec(compile(open(package_name + '/version.py').read(),
              package_name + 'src/version.py', 'exec'))
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 config = {
     'author'                 : '',
@@ -26,7 +48,9 @@ config = {
          'pyside',
          'pyevolve',
     ],
-   'name'                    : 'PKP'
+   'name'                    : 'PKP',
+   'tests_require' : ['pytest'],
+   'cmdclass' :  {'test': PyTest},
 }
 
 
---
+++ tests/test_main.py
@@ -0,0 +1,77 @@
+import os
+
+import PKP.src.CPD_SetAndLaunch as cpdsl
+import PKP.src.Models as mdls
+import numpy as np
+from PKP.PKP import MainProcess
+
+def test_calcC0_zeroTest():
+    """ Test if c0=0.0 for no carbon and no Ox """
+    assert cpdsl.CPD.calcC0(0.0, 0.0) == 0.0
+
+def test_calcC0_maximumTestCarbon():
+    """ Test if for 100% carbon the maximum c0=0.36 is returned """
+    assert cpdsl.CPD.calcC0(1.0, 0.0) == 0.36
+
+def test_calcC0_maximumTestOxygen():
+    """ Test if for 100% oxygen the maximum c0=0.15 is returned """
+    assert cpdsl.CPD.calcC0(0.0, 1.0) == 0.15
+
+def test_calcC0_website_8515():
+    """ Test if for 15%oxygen and 85% carbon a value of 0.035 is returned.
+        Value has been calculated with the online calculator tool 
+        from http://www.et.byu.edu/~tom/cpd/correlation.html """
+    assert cpdsl.CPD.calcC0(0.85, 0.15) == 0.035
+
+def test_calcCoalParam():
+    """ Test if for 
+            10% oxygen and 
+            70% carbon 
+            3%  hydrogen
+            2%  nitrogen
+            15% vm 
+        mdel == 43.3
+        MW per cluster == 551.2
+        Po == 0.65
+        sigma+1 ==  5.91
+        c0 == 0.0
+        Value has been calculated with the online calculator tool 
+        from http://www.et.byu.edu/~tom/cpd/correlation.html """
+
+    ret = cpdsl.CPD.CalcCoalParam(
+        {'Carbon': 0.7, 'Hydrogen': 0.03,
+         'Oxygen': 0.1, 'Nitrogen': 0.2},
+        {'Volatile Matter': 0.15})
+
+    expected = { 'c0'   : 0.0,
+                 'mdel' : 43.27,
+                 'mw'   : 551.16,
+                 'sig'  : 5.91,
+                 'p0'   : 0.65, }
+    for key, value in expected.iteritems():
+        assert abs(ret[key] - value) < 0.01
+
+def test_calcMassCR():
+    """ 
+
+    """
+    const_rate = mdls.ConstantRateModel({"k":1.0,"tstart":0.0,"finalYield":1.0})
+    # Test if at t=0 no mass is released
+    assert const_rate.calcMass(0.0,0.0) == 0.0
+ 
+
+# def test_modelError():
+#     res = mdls.Model.modelError(
+#                 np.array(range(10)),
+#                 np.array(range(10)))
+#     assertIs(type(res), np.float64)
+#     assertAlmostEqual(res, 0.0)
+#
+#
+
+def test_main(tmpdir):
+    mp = MainProcess(inputs_folder =  "/home/go/documents/code/pkp.git/inputs/")
+    res = mp.executeSolver()
+    mp.postProcessResults(res)
+
+
