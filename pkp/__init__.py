7d1128e4f0fa8318318b35f933c9d00c454b7d79
gregor <gregor.olenik@web.de>
2015-01-20 11:29:46 +0100

    corrected imports, fixed typo in docopt, removed recursive main call

--- pkp/__init__.py
+++ pkp/__init__.py
@@ -3,9 +3,9 @@
 PKP
 
 Usage:
-      PKP.py -h | --help
-      PKP.py generate (--json-input=<string> | --file-input=<loc>) --results-folder=<loc>
-      PKP.py fit      (--json-input=<string> | --file-input=<loc>) --fit-target=<string> --results-folder=<loc>
+      pkp-cli -h | --help
+      pkp-cli generate (--json-input=<string> | --file-input=<loc>) --results-folder=<loc>
+      pkp-cli fit      (--json-input=<string> | --file-input=<loc>) --fit-target=<string> --results-folder=<loc>
 
 Options:
     -h  --help              Shows this screen
@@ -15,20 +15,17 @@ Options:
     --fit-target=<string>   Bla
 
 """
-try:
-    import PKP.src
-except:
-    import src
 import sys
 import os
 import platform
 from docopt import docopt
+
+import src
+from pkpcli import generate
+from pkpcli import fit
 
 def main():
     arguments = docopt(__doc__)
     print arguments
     if arguments['generate']:
         generate(json_string=arguments['--json-input'], folder=arguments['--file-input'])
-    main()
-    
-    
