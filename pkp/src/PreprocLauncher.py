f4e58aaea5c413ddd42e7f59bc496fe2315ea568
gregor <gregor.olenik@web.de>
2015-06-16 08:25:09 +0200

    Check for runs keyword in input file to set number of runs explicitly, or iterate over all run keys

--- pkp/src/PreprocLauncher.py
+++ pkp/src/PreprocLauncher.py
@@ -16,7 +16,8 @@ def Launch_CPD(inputs, output_folder):
      """
     def InitAndLaunch(*pargs,**kwargs):
         """ initialises and execute cpd calculation """
-        print 'Running CPD: ' + kwargs.get('runNr')
+        if kwargs.get('verbose', False):
+            print 'Running CPD: ' + kwargs.get('runNr')
         cpd = CPD.CPD(*pargs)
         return cpd.Run()
 
@@ -25,13 +26,20 @@ def Launch_CPD(inputs, output_folder):
     pressure = operatingConditions['pressure']
     from CoalThermoPhysics import Coal
     coal = Coal(inputs["Coal"])
-    return {'run'+str(run):(operatingConditions["run"+str(run)], InitAndLaunch(
+    # Check if number of runs is demanded explicitly
+    # if not iterate over all run keys
+    runs_exp = operatingConditions.get('runs')
+    if runs_exp:
+        runs = ['run' + str(i) for i in range(runs_exp)]
+    else:
+        runs = [runNr for runNr in operatingConditions.keys()
+                    if 'run' in runNr ]
+    return {run: (operatingConditions[run], InitAndLaunch(
                 coal,
-                operatingConditions["run"+str(run)],
+                operatingConditions[run],
                 pressure,
                 inputs['CPD']['deltaT'],
                 run,
                 output_folder,
                 runNr=str(run))
-        ) for run in range(operatingConditions['runs'])}
-
+        ) for i, run in enumerate(runs)}
