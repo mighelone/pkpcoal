072139d6a6654ea0dddbf72db82a166ddb8f9e0e
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2016-09-05 12:26:03 +0200

    Use setuptool for install package

--- setup.py
+++ setup.py
@@ -1,18 +1,53 @@
-from distutils.core import setup
+'''
+https://github.com/pypa/sampleproject/blob/master/setup.py
+'''
+
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='PKP',
     version='2.0.0',
-    packages=['pkp', 'pkp.bins'],
+    description='Pyrolysis Kinetic Preporcessor',
+    long_description=long_description,
+    url='',
+    author='Michele Vascellari',
+    author_email='Michele.Vascellari@vtc.tu-freiberg.de',
+    license='',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: ',
+
+        # Specify the Python versions you support here.
+        # In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or
+        # both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    # packages=['pkp', 'pkp.bins'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     packages_dir={'pkp.bins': 'pkp/bins'},
     package_data={'pkp.bins': ['cpdnlg*', 'COAL.xml', 'Biomass.xml']},
     scripts=['runPKP'],
-    url='',
-    license='',
-    author='Michele Vascellari',
-    author_email='Michele.Vascellari@vtc.tu-freiberg.de',
-    description='Pyrolysis Kinetic Preporcessor',
-    requires=[
+    install_requires=[
         'numpy',
         'matplotlib',
         'scipy',
