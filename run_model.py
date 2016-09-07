f521d58649182ff0b994e7d292bb8bf918aea9dc
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2016-09-07 07:08:34 +0200

    Remove useless runner test

--- run_model.py
+++
@@ -1,8 +0,0 @@
-import pkp.empirical_model
-import numpy as np
-par_min = np.array([1e3, 10e6, 0.4])
-par_max = np.array([1e8, 200e6, 0.6])
-scal_par = np.array([0.2, 0.5, 0.4])
-
-unsc_par = pkp.empirical_model.SFOR.unscale_parameters(
-    scal_par, par_min, par_max)
